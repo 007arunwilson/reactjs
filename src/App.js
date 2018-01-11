681234c9737c71c065719646df3645a4d09dee2b
arun <arun@travalour.com>
2018-01-12 00:05:45 +0530

    Setting States to Components and accessing it in JSX

--- src/App.js
+++ src/App.js
@@ -4,14 +4,25 @@ import Person from './Person/Person';
 import Animal from './Animal/Animal';
 
 class App extends Component {
+
+  state = {
+    persons:[
+      {name:'Madison',age:26},
+      {name:'Davidson',age:27},
+      {name:'Pearson',age:28},
+      {name:'Paulson',age:20},
+    ]
+  }
+
   render() {
     return (
       <div title="Core base element" id="_core_element" className="App">
       <h1>Starting New React App !...</h1>
       <p>This is really working, and its awesome too ...</p>
-      <Person name="Arun" age="25"/>
-      <Person name="Varun" age="25" >Brilliant Boy</Person>
-      <Person name="Kiran" age="27" />
+      <button>Change Value</button>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       {/*<Animal/>*/}
       </div>
       // <h1>Another Element</h1>
