4bd3a07992e35c7ac7eea49578c6292c4ab70b88
arun <arun@travalour.com>
2018-01-12 01:16:31 +0530

    On Click Handler and State changes Implemented

--- src/App.js
+++ src/App.js
@@ -14,12 +14,24 @@ class App extends Component {
     ]
   }
 
+switchNameEventHandler = () => {
+  //console.log('Was CLicked');
+  //Don't do this // this.state.persons[0].name = 'Madison Madi';
+  this.setState({persons:[
+    {name:'Madison Madi',age:26},
+    {name:'Davidson David',age:27},
+    {name:'Pearson Pear',age:28},
+    {name:'Paulson Paul',age:20},
+  ]})
+}
+
+
   render() {
     return (
       <div title="Core base element" id="_core_element" className="App">
       <h1>Starting New React App !...</h1>
       <p>This is really working, and its awesome too ...</p>
-      <button>Change Value</button>
+      <button onClick={this.switchNameEventHandler} >Change Value</button>
       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
       <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
       <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
