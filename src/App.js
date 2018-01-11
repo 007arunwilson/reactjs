a7f9bd7ec847ce456199aaf4abbaeaea45f975bb
arun <arun@travalour.com>
2018-01-11 22:31:37 +0530

    Created Person component;

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //   <h1>Starting New React App !...</h1>
-    //   </div>
-    // );
+    return (
+      <div title="Core base element" id="_core_element" className="App">
+      <h1>Starting New React App !...</h1>
+      <p>This is really working, and its awesome too ...</p>
+      <Person/>
+      </div>
+      // <h1>Another Element</h1>
+    );
 
-    return React.createElement('div',{className:'App'},React.createElement('h1',null,'Starting React App !...'));
+    //return React.createElement('div',{className:'App'},React.createElement('h1',null,'Starting React App !...'));
 
   }
 }
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const person = () => {
+  return <p>Im a from person component</p>
+};
+
+export default person;
