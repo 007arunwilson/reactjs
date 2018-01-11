9793abcdbb77ead72642d7aafdd57f79b0bcf79b
arun <arun@travalour.com>
2018-01-11 21:39:46 +0530

    Returning Elements with React basic  methods;

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-      <h1>Starting New React App ...</h1>
-      </div>
-    );
+    // return (
+    //   <div className="App">
+    //   <h1>Starting New React App !...</h1>
+    //   </div>
+    // );
+
+    return React.createElement('div',{className:'App'},React.createElement('h1',null,'Starting React App !...'));
+
   }
 }
 
