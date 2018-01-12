e3f922617d5a851fde5d6b5bca18b91a72eeeac7
arun <arun@travalour.com>
2018-01-12 22:46:08 +0530

    Using Internal Css in React

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
   return (
-    <div onClick={props.click} >
+    <div className="person" onClick={props.click} >
     <p><b>{props.name} </b>from person,with {props.age} years old
     <br/>
       <small><i>Quote : {props.children}</i></small>
     </p>
-    <input onChange={props.onchangefn} type="text"/>
+    <input onChange={props.onchangefn} value={props.name} type="text"/>
     </div>
   )
 };
